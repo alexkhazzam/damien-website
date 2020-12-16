09cfcd78257a319cbeedabe8815dc53957a44f62
alexkhazzam <alex722khazzam@gmail.com>
2020-12-16 14:54:25 -0500

    making it bettr

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "config.env" });
 
-const PORT = process.env.PORT || 5000; // optional ports
+const PORT = process.env.PORT || 3000; // optional ports
 
 app.listen(PORT, () => {
   console.log(`Listening to requests on port ${PORT}`);
