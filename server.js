3a44d91ed16037d414c029c9d7a6786eebb3eb6d
alexkhazzam <alex722khazzam@gmail.com>
2020-12-16 14:49:35 -0500

    adding optional ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "config.env" });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000; // optional ports
 
 app.listen(PORT, () => {
   console.log(`Listening to requests on port ${PORT}`);
