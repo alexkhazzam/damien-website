a9ff0398479f24f485f9170c00b466aff22514b5
alexkhazzam <alex722khazzam@gmail.com>
2020-12-16 14:47:24 -0500

    adding stuff

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -1,19 +1,19 @@
-const writeableTextWrapper = document.querySelector(".writeable-text-wrapper");
-const writeableText = document.querySelector(".writeable-text");
+// const writeableTextWrapper = document.querySelector(".writeable-text-wrapper");
+// const writeableText = document.querySelector(".writeable-text");
 
-const text = "Damien Chafe's Tennis Academy";
+// const text = "Damien Chafe's Tennis Academy";
 
-let i = 0;
-function outputText() {
-  if (i === 29) {
-    return;
-  } else {
+// let i = 0;
+// function outputText() {
+//   if (i === 29) {
+//     return;
+//   } else {
 
-  }
-}
+//   }
+// }
 
-function blob(setTimeout(() => {
-    
-}, timeout);)
+// function blob(setTimeout(() => {
 
-outputText();
+// }, timeout);)
+
+// outputText();
